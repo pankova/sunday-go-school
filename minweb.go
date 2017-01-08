3fd0743148c1c92f29d873812d586ab755c14205
pankova <flam.mary@gmail.com>
2017-01-08 20:42:55 +0700

    responce hello name, where name is param in GET

--- minweb.go
+++ minweb.go
@@ -7,14 +7,12 @@ import (
 ) 
 
 func main() { 
-	var name string = "test"
-	http.HandleFunc("/hello", func (w http.ResponseWriter, r *http.Request) {
-		handler(w, r, name)
-	}) // each request contains /hello calls handler 
+	http.HandleFunc("/hello", handler) // each request contains /hello calls handler 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil)) 
 } 
 
-// handler echoes the Path component of the requested URL. 
-func handler(w http.ResponseWriter, r *http.Request, name string) {
-	fmt.Fprintf(w, "Hello, %q\n", name) 
+// handler echoes the Path component of the requested URL 
+func handler(w http.ResponseWriter, r *http.Request) {
+	var name string = r.FormValue("name")
+	fmt.Fprintf(w, "Hello, %v!\n", name) 
 }
