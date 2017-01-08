7103ed30a1f549d3aab32a5ba57565243c577d1f
pankova <flam.mary@gmail.com>
2017-01-08 20:29:27 +0700

    print const string on hello request

--- minweb.go
+++ minweb.go
@@ -7,11 +7,14 @@ import (
 ) 
 
 func main() { 
-	http.HandleFunc("/", handler) // each request calls handler 
+	var name string = "test"
+	http.HandleFunc("/hello", func (w http.ResponseWriter, r *http.Request) {
+		handler(w, r, name)
+	}) // each request contains /hello calls handler 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil)) 
 } 
 
 // handler echoes the Path component of the requested URL. 
-func handler(w http.ResponseWriter, r *http.Request) { 
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path) 
+func handler(w http.ResponseWriter, r *http.Request, name string) {
+	fmt.Fprintf(w, "Hello, %q\n", name) 
 }
